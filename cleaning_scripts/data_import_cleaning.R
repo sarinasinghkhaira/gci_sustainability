68245bba40fbad0b5c971e151760436f7cb6aeb9
Sarina Singh Khaira <sarina.k92@gmail.com>
2021-03-04 12:41:50 +0000

    cleaned up GSCI data

--- cleaning_scripts/data_import_cleaning.R
+++ cleaning_scripts/data_import_cleaning.R
@@ -2,30 +2,32 @@ library(tidyverse)
 library(readxl)
 library(janitor)
 library(snakecase)
+library(countrycode)
 
 
-# Reading in data ---------------------------------------------------------
-#https://databank.worldbank.org/source/world-development-indicators
-world_bank_data <- read_csv("raw_data/World_Bank_Data.csv") %>%
-  clean_names()
-
-#
-gci_data <- read_excel("raw_data/Global Competitiveness Dataset.xlsx", 
-                       sheet = "Data", skip = 3)
+# Obtain countries of interest --------------------------------------------
 
 gci_countries <- read_excel("raw_data/Global Competitiveness Dataset.xlsx", 
                             sheet = "Entities" , skip = 2) %>%
   clean_names()
 
-# https://tcdata360.worldbank.org/indicators/gci
-gci_decade <- read_csv("raw_data/gci_historic_data.csv") %>%
-  clean_names()
+# Filter out aggregates
+gci_countries <- gci_countries %>%
+  select(entity_name, entity_code) %>%
+  filter(!entity_code %in% c("AVG", "LIC", "LMC", "UMC", "HIC", "GCREAP", 
+                             "GCRMENA", "GCRLATAM", "GCREUROPENA", 
+                             "GCREURASIA", "GCRSSA", "GCRSASIA")) %>%
+  distinct()
 
-#https://ourworldindata.org/co2-and-other-greenhouse-gas-emissions
-co2_emissions <- read_csv("raw_data/owid-co2-data.csv")
+gci_country_names <- pull(gci_countries %>% distinct(entity_name))
+gci_country_iso <- pull(gci_countries %>% distinct(entity_code))
+
 
 
 # Clean GCI Data from folder --------------------------------------
+
+gci_data <- read_excel("raw_data/Global Competitiveness Dataset.xlsx", 
+                       sheet = "Data", skip = 3)
 
 # remove columns with one distinct value
 gci_data <- gci_data %>%
@@ -62,46 +64,23 @@ gci_data_clean <- gci_data_clean %>%
   # remove redundant columns
   select(c(country, date_description, series_name, sample_average, score, rank))
 
-# select out the scores for each pillar
+
+# join iso codes
+gci_data_clean <- gci_data_clean %>%
+  left_join(gci_countries, by = c("country" = "entity_name")) %>%
+  rename(country_code = entity_code)
+
+# select out the scores for each pillar and GCI index
 gci_pillars <- gci_data_clean %>%
-  filter(str_detect(series_name, "[Pp]illar")) 
-
-# create vector of countries of interest to pull from other datasets
-gci_countries <- gci_countries %>%
-  inner_join(gci_pillars, by = c("entity_name" = "country")) %>%
-  select(entity_name, entity_code) %>%
-  distinct()
-  
-gci_country_names <- pull(gci_countries, entity_name)
-gci_country_iso <- pull(gci_countries, entity_code)
-
-# GCI rankings ------------------------------------------------------------
-
-gci_decade <- gci_decade %>%
-  pivot_longer( cols = starts_with("x20"),
-                names_to = "year",
-                names_prefix = "x") %>%
-  filter(!is.na(value))
-
-gci_rankings <- gci_decade %>% 
-  filter(indicator == "Global Competitiveness Index") %>%
-  mutate(year = str_extract(year, "20[0-9][0-9]$")) %>%
-  select(-c(indicator_id, indicator)) %>%
-  pivot_wider(names_from = subindicator_type,
-              values_from = value,
-              names_prefix = "gci_") %>%
-  clean_names()
-
-gci_data %>%
- filter(str_detect(`Series name`, "GDP")) %>% distinct(`Series name`)
-
-
-# Obtain GDP figures ------------------------------------------------------
-
+  filter(str_detect(series_name, "[Pp]illar")| str_detect(series_name, "Index 4.0"))
 
 
 
 # Clean World Bank Data ---------------------------------------------------
+
+#https://databank.worldbank.org/source/world-development-indicators
+world_bank_data <- read_csv("raw_data/World_Bank_Data.csv") %>%
+  clean_names()
 
 # Replace ".." with NA
 world_bank_data <- world_bank_data %>% 
@@ -139,12 +118,111 @@ world_bank_clean <- world_bank_pvt %>%
 
 # Clean Carbon Data -------------------------------------------------------
 
+#https://ourworldindata.org/co2-and-other-greenhouse-gas-emissions
+co2_emissions <- read_csv("raw_data/owid-co2-data.csv")
+
 co2_emissions <- co2_emissions %>%
   filter(iso_code %in% gci_country_iso &
-         year > 2009)
+         year > 2009) %>%
+  rename(country_code = iso_code)
+
+co2_trimmed <- co2_emissions %>%
+  select(country, year, country_code, co2, consumption_co2_per_capita)
+
+
+# Obtain GDP figures ------------------------------------------------------
+
+#https://databank.worldbank.org/reports.aspx?source=2&series=NY.GDP.PCAP.KN&country=#advancedDownloadOptions
+gdp_data <- read_csv("raw_data/gdp_data.csv") %>%
+  clean_names()
+
+gdp_clean <- gdp_data %>%
+  na_if(., "..") %>%
+  filter(country_code %in% gci_country_iso) %>%
+  pivot_longer( cols = starts_with("x20"),
+                names_to = "year",
+                names_prefix = "x") %>%
+  mutate(year = as.numeric(str_extract(year, "20[0-9][0-9]")),
+         value = as.numeric(value)) %>%
+  filter(!is.na(series_name), 
+         !str_detect(series_name, "Last Updated"), 
+         !str_detect(series_name, "Data from database")) %>%
+  rename(country = country_name)
+
+
+gdp_clean <- gdp_clean %>%
+  mutate(series_name = to_snake_case(series_name), 
+         series_name = str_remove_all(series_name, "current_"),
+         series_name = str_remove_all(series_name, "_international")) %>%
+  select(-series_code) %>%
+  pivot_wider(names_from = series_name,
+              values_from = value)
+
+gdp_avg_growth <- gdp_clean %>%
+  filter(year <= 2018) %>%
+  group_by(country_code) %>%
+  summarise(
+    gdp_growth_decade = mean(gdp_growth_annual, na.rm = TRUE)
+  )
+
+
+# Global Gender Gap Index -------------------------------------------------
+# https://tcdata360-backend.worldbank.org/api/v1/datasets/743/dump.csv
+# Index between 0 (inequality) and 1 (equality)
+
+ggi_data <- read_csv("raw_data/gender_gap_index.csv") %>%
+  clean_names()
+
+ggi_data_clean_full <- ggi_data %>%
+  na_if(., "..") %>%
+  pivot_longer( cols = starts_with("x20"),
+                names_to = "year",
+                names_prefix = "x") %>%
+  mutate(year = as.numeric(str_extract(year, "20[0-9][0-9]")),
+         value = as.numeric(value)) %>%
+  select(-indicator_id) %>%
+  pivot_wider(names_from = subindicator_type,
+             values_from = value,
+             names_prefix = "ggi_") %>%
+  clean_names()
+
+
+ggi_clean_filter <- ggi_data_clean_full %>%
+  rename(country = country_name,
+         country_code = country_iso3) %>%
+  filter(country_code %in% gci_country_iso,
+         !is.na(ggi_index),
+         indicator == "Overall Global Gender Gap Index") %>%
+  select(-ggi_normalized_score, -indicator) %>%
+  group_by(country_code) %>%
+  filter(year == max(year))
 
 
 
+
+# Natural Capital GCSI 2020 ----------------------------------------------------
+# https://solability.com/the-global-sustainable-competitiveness-index/the-index/natural-capital
+gcsi_data <- read_excel("raw_data/GSCI_Scores_2020.xlsx", skip = 2) %>%
+  clean_names()
+
+# names(gcsi_data) <- paste(names(gcsi_data), str_to_lower(gcsi_data[1, ]), sep = "_")
+
+names(gcsi_data) <- c("country", 
+                      "gsci_rank", "gsci_score", 
+                      "nat_cap_rank", "nat_cap_score",
+                      "res_intense_rank", "res_intense_score",
+                      "soc_cap_rank", "soc_cap_score",
+                      "intellect_cap_rank", "intellect_cap_score",
+                      "governance_rank", "governance_score")
+
+gcsi_data <- gcsi_data %>%
+  slice(-1) %>%
+  mutate(across(-country, as.numeric),
+         country_code = countryname(country, destination = "iso3c")) %>%
+  filter(country_code %in% gci_country_iso)
+
+natural_cap <- gcsi_data %>%
+  select(country, country_code, gsci_rank, gsci_score, nat_cap_rank, nat_cap_score)
 
 # Join data sets for cluster analysis -------------------------------------
 # For cluster analysis we need data from 2018 for each pillar and gdp
@@ -153,17 +231,58 @@ ca_gci_pillars <- gci_pillars %>%
   filter(date_description == "2018 edition") %>%
   mutate(series_name = str_remove(series_name, "illar "),
          series_name = str_extract(series_name, "P[0-9]*: [a-zA-Z0-9]*"),
-         series_name = to_snake_case(series_name)) %>%
-  select(country, series_name, score) %>%
+         series_name = to_snake_case(series_name),
+         series_name = replace_na(series_name, "gci_overall")) %>%
+  select(country_code, series_name, score) %>%
   pivot_wider(names_from = series_name,
               values_from = score)
 
-ca_gci <- gci_rankings %>%
+
+ca_gdp <- gdp_clean %>%
   filter(year == "2018") %>%
-  select(country_name, gci_value)
+  select(-c(year, country))
 
 
-######### Notes for tomorrow:
-# ensure every dataset has ISO code in it from the start, this is how you will join
-# obtain the 141 countries in the report and make sure you have data for all of them
-# put together the cluster analysis table where there is one row for each country
+ca_join <- ca_gci_pillars %>%
+  inner_join(ca_gdp) %>%
+  inner_join(gdp_avg_growth) %>%
+  mutate(across(-country_code, as.numeric))
+
+# Check for NAs  ----------------------------------------------------------
+ca_join %>%
+  filter(is.na(ca_join))
+
+
+# Write to CSV ------------------------------------------------------------
+
+write_csv(ca_join, "clean_data/gdp_gci_clustering.csv")
+
+
+
+# GCI rankings for bump plot-----------------------------------------------
+
+# https://tcdata360.worldbank.org/indicators/gci
+gci_decade <- read_csv(here("raw_data/gci_historic_data.csv")) %>%
+  clean_names()
+
+gci_decade <- gci_decade %>%
+  pivot_longer( cols = starts_with("x20"),
+                names_to = "year",
+                names_prefix = "x") %>%
+  filter(!is.na(value)) %>%
+  rename(country = country_name,
+         country_code = country_iso3) %>%
+  mutate(country = str_remove_all(country, " SAR, China"))
+
+gci_rankings <- gci_decade %>%
+  filter(indicator == "Global Competitiveness Index") %>%
+  mutate(year = str_extract(year, "20[0-9][0-9]$")) %>%
+  select(-c(indicator_id, indicator)) %>%
+  pivot_wider(names_from = subindicator_type,
+              values_from = value,
+              names_prefix = "gci_") %>%
+  clean_names()
+
+
+
+write_csv(gci_rankings, "clean_data/gci_rank_decade.csv")
